fcc520ee2a767eb6dbd73d9ad3cdcf434f8e6d7c
Friedem <friede80@gmail.com>
2017-11-07 17:17:03 -0500

    Code is very messy, but you can sorta connect between two clients now

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,90 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
 import Graphics.UI.Gtk
+import Graphics.UI.Gtk.Builder
+import Control.Monad
+import Control.Monad.IO.Class
+import Network.Socket
+import System.IO
+import Control.Concurrent
 
+
+data GUI =
+  GUI { mainWin :: Window
+      , sendBtn :: Button
+      , msgEntry :: Entry
+      --, convList :: ListBox
+      , msgView :: TextView
+      , hostBtn :: MenuItem
+      , clientBtn :: MenuItem }
+
+main :: IO ()
 main = do
   initGUI
-  window <- windowNew
-  widgetShowAll window
+  gui <- loadGlade "GUI.glade"
+  initialCallbacks gui
+  widgetShowAll $ mainWin gui
   mainGUI
+
+initHostConnection :: GUI -> IO ()
+initHostConnection GUI{..} = do
+  msgDialog <- messageDialogNew Nothing [] MessageInfo ButtonsOk "Awaiting Connection"
+  dialogRun msgDialog
+
+  sock <- socket AF_INET Stream 0
+  setSocketOption sock ReuseAddr 1
+  bind sock (SockAddrInet 4242 iNADDR_ANY)
+  listen sock 2
+  (sock', _) <- accept sock
+  hdl <- socketToHandle sock' ReadWriteMode
+  hSetBuffering hdl NoBuffering
+  sendBtn `on` buttonActivated  $ sendMsg hdl msgEntry msgView
+  forkIO $ recieveThread hdl
+  return ()
+
+initClientConnection :: GUI -> String -> IO ()
+initClientConnection GUI{..} ipAddr = do
+  sock <- socket AF_INET Stream 0
+  addr <- inet_addr ipAddr
+  connect sock (SockAddrInet 4242 addr)
+  hdl <- socketToHandle sock ReadWriteMode
+  sendBtn `on` buttonActivated  $ sendMsg hdl msgEntry msgView
+  forkIO $ recieveThread hdl
+  return ()
+
+recieveThread :: Handle -> IO ()
+recieveThread hdl = forever $ hGetLine hdl >>= putStrLn
+
+initialCallbacks :: GUI -> IO ()
+initialCallbacks gui@GUI{..} = do
+  mainWin `on` deleteEvent $ quitApp
+  hostBtn `on` menuItemActivated  $ initHostConnection gui
+  clientBtn `on` menuItemActivated  $ initClientConnection gui "127.0.0.1"
+  --sendBtn `on` buttonActivated  $ sendMsg sockHdl msgEntry msgView
+  return ()
+
+sendMsg :: Handle -> Entry -> TextView -> IO ()
+sendMsg sockHdl entry view = do
+  msg <- entryGetText entry
+  entrySetText entry ""
+  hPutStrLn sockHdl msg
+
+loadGlade :: String -> IO GUI
+loadGlade gladeFP = do
+  builder <- builderNew
+  builderAddFromFile builder gladeFP
+  mw <- builderGetObject builder castToWindow "MainWindow"
+  sb <- builderGetObject builder castToButton "SendBtn"
+  me <- builderGetObject builder castToEntry "MsgEntry"
+  --cl <- builderGetObject builder castToListBox "ConvList"
+  mv <- builderGetObject builder castToTextView "MsgView"
+  hb <- builderGetObject builder castToMenuItem "HostBtn"
+  cb <- builderGetObject builder castToMenuItem "ClientBtn"
+
+  return $ GUI mw sb me  mv hb cb
+
+
+quitApp :: EventM a Bool
+quitApp = liftIO mainQuit >> return False
